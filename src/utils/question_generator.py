2b1b244f0220a2d0a8faa2e10629da923ff4cd51
0KeirLi0 <158504174+0KeirLi0@users.noreply.github.com>
2025-03-26 12:59:42 +0800

    Add Question Topic

--- src/utils/question_generator.py
+++ src/utils/question_generator.py
@@ -11,13 +11,14 @@ class AnsFormmat(BaseModel):
     topic: str
 
 # Behavioral topics
-topics = ["Motivation and Fit for Finance",
-          "Teamwork and Collaboration",
-          "Problem-Solving and Analytical Thinking",
-          "Handling Pressure and Time Management",
-          "Leadership and Initiative",
-          "Adaptability and Learning from Feedback",
-          "Ethical Judgment and Integrity"]
+topics = ["Motivation - What drives you to Financial Industry? Ideal candidates are self-motivated, passionate about technologies and products that have a real impact.",
+          "Ability to be Proactive - Are you able to take initiative? Given a difficult problem, are you able to figure out how to get it done and execute on it?",
+          "Ability to work in an unstructured environment - How well are you able to take ownership in ambiguous situations? Or do you rely on others to be told what to do?",
+          "Perseverance - Are you able to push through difficult problems or blockers?",
+          "Conflict Resolution - How well are you able to handle and work through challenging relationships?",
+          "Empathy - How well are you able to see things from the perspective of others and understand your motivations?",
+          "Growth - How well do you understand your strengths, weaknesses and growth areas? Are you making a continued effort to grow?",
+          "Communication - Are you able to clearly communicate your stories during the interview?"]
 
 def generate_behavioral_question(background: str, client: OpenAI, model: str = "gpt-4o-mini"):
     """
